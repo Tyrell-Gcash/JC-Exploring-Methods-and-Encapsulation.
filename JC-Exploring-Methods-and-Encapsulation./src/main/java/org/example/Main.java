f0a9ff86d5e81956dddc5fedb85c3e63d837b4f7
Tyrell Teppang <tyrellmarcus.teppang@gcash.com>
2025-08-11 16:45:29 +0800

    Lab Done

--- JC-Exploring-Methods-and-Encapsulation./src/main/java/org/example/Main.java
+++ JC-Exploring-Methods-and-Encapsulation./src/main/java/org/example/Main.java
@@ -86,6 +86,39 @@ public class Main {
 //        System.out.println(25.0 + " degrees C is " + Converter.celsiusToFahrenheit(25.0)  + " degrees F.");
 //        System.out.println(77.0 + " degrees F is " + Converter.fahrenheitToCelsius(77.0)  + " degrees C.");
 
+//        //7. Constructors and Constructor Overloading
+//        Pizza pizza1 = new Pizza();
+//        pizza1.display();
+//
+//        Pizza pizza2 = new Pizza("Pepperoni");
+//        pizza2.display();
+//
+//        Pizza pizza3 = new Pizza("Mushroom and Onion", 16);
+//        pizza3.display();
+
+//        Computer comp1 = new Computer("Generic", 8);
+//        Computer comp2 = new Computer("Dell", 8);
+//        Computer comp3 = new Computer("Apple", 16);
+//
+//        System.out.println("Computer 1:" + comp1.getDetails());
+//        System.out.println("Computer 2:" + comp2.getDetails());
+//        System.out.println("Computer 3:" + comp3.getDetails());
+
+//        //8. Encapsulation
+//        Thermostat stat = new Thermostat(22.5);
+//        System.out.println("Initial temp: " + stat.getTemperatureCelsius());
+//
+//        stat.setTemperatureCelsius(5.0); // Try to set an invalid temp
+//        System.out.println("Temp after invalid change: " + stat.getTemperatureCelsius());
+//
+//        stat.setTemperatureCelsius(25.0); // Set a valid temp
+//        System.out.println("Temp after valid change: " + stat.getTemperatureCelsius());
+        User user = new User("12345678");
+        System.out.println(user.getPassword());
+        user.setPassword("pass");
+        System.out.println(user.getPassword());
+        user.setPassword("secure_password_123");
+
 
     }
 }
@@ -205,5 +238,102 @@ class Display {
 
     public void show(int number) {
         System.out.println("Number: " + number);
+    }
+}
+
+class Pizza {
+    private String toppings;
+    private int size; // in inches
+
+    public Pizza() {
+        this.toppings = "Cheese";
+        this.size = 12;
+        System.out.println("Default pizza created.");
+    }
+
+    public Pizza(String toppings) {
+        this(); // Calls the no-argument constructor first
+        this.toppings = toppings;
+        System.out.println("Pizza with custom toppings created.");
+    }
+
+    public Pizza(String toppings, int size) {
+        this.toppings = toppings;
+        this.size = size;
+        System.out.println("Fully custom pizza created.");
+    }
+
+    public void display() {
+        System.out.println("Size: " + size + " inches, Toppings: " + toppings);
+    }
+}
+
+class Computer {
+    private String brand;
+    private int ramInGB;
+
+    public Computer() {
+        this("Generic"); // Calls constructor with brand only
+    }
+
+    public Computer(String brand) {
+        this(brand, 8); // Calls constructor with brand and RAM
+    }
+
+    public Computer(String brand, int ramInGB) {
+        this.brand = brand;
+        this.ramInGB = ramInGB;
+    }
+
+    public String getDetails() {
+        return "Brand = " + brand + ", RAM = " + ramInGB + "GB";
+    }
+}
+
+class Thermostat {
+    private double temperatureCelsius;
+
+    public Thermostat(double temp) {
+        // Use the setter in the constructor to enforce rules from the start
+        setTemperatureCelsius(temp);
+    }
+
+    public double getTemperatureCelsius() {
+        return this.temperatureCelsius;
+    }
+
+    public void setTemperatureCelsius(double temp) {
+        if (temp >= 10.0 && temp <= 30.0) {
+            this.temperatureCelsius = temp;
+        } else {
+            System.out.println("Error: Temperature must be between 10.0 and 30.0 Celsius.");
+        }
+    }
+}
+
+class User{
+    private String password;
+
+    public User(String password){
+        setPassword(password);
+    }
+
+    public String getPassword(){
+        String password;
+        password = "Current masked password: " + "*".repeat(this.password.length());
+
+        return password;
+    }
+
+    public void setPassword(String password){
+
+        System.out.println("Attempting to set valid password '" + password + "'...");
+        if(password.length() < 8){
+            System.out.println("Error: Password must be at least 8 characters long.");
+        }
+        else{
+            this.password = password;
+        }
+
     }
 }
